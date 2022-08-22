77b66a8a5c95ba4a7058ad48ebabe112fdc813dd
GaliaBonda <galina.rizhniak@gmail.com>
2022-08-22 16:27:30 +0300

    get first 20 characters

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,28 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { sagaActions } from '../../store/sagas';
+import { RootState } from '../../store/store';
 
 function Main() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch({ type: sagaActions.UPDATE_CHARACTERS_SAGA });
   }, []);
-  return <div>main</div>;
+  const characters = useSelector((state: RootState) => state.characters);
+  return (
+    <div>
+      <ul className='characters'>
+        {characters.map((item) => {
+          return (
+            <li key={item.id}>
+              <p>{item.name}</p>
+              <img src={item.image} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Main;
--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -1,20 +1,24 @@
-import { put, takeEvery, call } from 'redux-saga/effects';
+import { put, takeEvery, call, all } from 'redux-saga/effects';
 import api from '../api/api';
 import ICharacterApi from '../common/interfaces/ICharacterApi';
 import IResponse from '../common/interfaces/IResponse';
 import { update } from '../features/characters/charactersSlice';
 
-export function* updateCharacters() {
+function* updateCharacters() {
   const data: IResponse<ICharacterApi> = yield call(() =>
     api.get('/character')
   );
   yield put(update(data.results));
 }
+function* watchUpdateUsers() {
+  yield takeEvery(sagaActions.UPDATE_CHARACTERS_SAGA, updateCharacters);
+}
+function* addCharacters() {}
 
 export const sagaActions = {
   UPDATE_CHARACTERS_SAGA: 'UPDATE_CHARACTERS_SAGA',
 };
 
 export default function* rootSaga() {
-  yield takeEvery(sagaActions.UPDATE_CHARACTERS_SAGA, updateCharacters);
+  yield all([watchUpdateUsers()]);
 }
