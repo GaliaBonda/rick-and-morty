1147affb41ae2a105672e03445c87caeb782b6ba
GaliaBonda <galina.rizhniak@gmail.com>
2022-08-24 11:54:08 +0300

    sorting for episodes needs testing

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ITableData from '../common/interfaces/ITableData';
 import TableRow from './TableRow';
 
 interface Props {
   header: string[];
   rows: ITableData[];
+  changeSort: (desc: boolean, column: number) => void;
 }
 
-function Table({ header, rows }: Props) {
+function Table({ header, rows, changeSort }: Props) {
+  const [descSorted, setDescSorted] = useState(false);
+  const handleClick = (index: number) => {
+    setDescSorted((prevState) => !prevState);
+    changeSort(!descSorted, index);
+  };
   return (
     <table>
       <thead>
         <tr>
           {header.map((item, index) => {
-            return <th key={index}>{item}</th>;
+            return (
+              <th key={index} onClick={() => handleClick(index)}>
+                {item}
+              </th>
+            );
           })}
         </tr>
       </thead>
--- src/pages/Episodes/Episodes.tsx
+++ src/pages/Episodes/Episodes.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Table from '../../components/Table';
 import { sagaActions } from '../../store/sagas';
@@ -6,9 +6,14 @@ import { RootState } from '../../store/store';
 
 function Episodes() {
   const characters = useSelector((state: RootState) => state.characters);
-  const episodesRows = characters.map((item) => {
-    return { id: item.id, data: [item.name, item.episode?.length || 0] };
-  });
+  const episodesRows = characters
+    .map((item) => {
+      return { id: item.id, data: [item.name, item.episode?.length || 0] };
+    })
+    .sort((a, b) => {
+      return a.data[1] <= b.data[1] ? 1 : -1;
+    });
+  const [sortedRows, setSortedRows] = useState(episodesRows);
 
   const dispatch = useDispatch();
 
@@ -30,10 +35,25 @@ function Episodes() {
   //     );
   //   }, []);
 
+  const changeSort = (desc: boolean, column: number) => {
+    setSortedRows((startRows) => {
+      if (desc) {
+        return [...startRows].sort((a, b) =>
+          a.data[column] <= b.data[column] ? 1 : -1
+        );
+      } else {
+        return [...startRows].sort((a, b) =>
+          a.data[column] >= b.data[column] ? 1 : -1
+        );
+      }
+    });
+  };
+
   return (
     <Table
       header={['Character name', 'Number of episodes']}
-      rows={episodesRows}
+      rows={sortedRows}
+      changeSort={changeSort}
     />
   );
 }
